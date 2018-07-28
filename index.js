4367f42a0be388ac421631846d006b6a0005cadc
Mabast Hashm Jamel <mabasthashm1@gmail.com>
2018-07-28 18:48:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
-/*
-Variables defined in the test suite:
-- oldID
-- currentAge
+const oldID = Math.floor(Math.random() * 100) + 1;
 
-You should NOT try to redefine any of the above variables.
-*/
+const ages = [26.4, 21, 49.5, 83, 99, 53.3, 74, 94.9, 40, 56.2];
 
-// Write your code below this line!
+const currentAge = ages[Math.floor(Math.random() * ages.length)];
+
+const currentAgeIsInteger = Number.isInteger(currentAge);
+
+const spyOnNumberIsInteger = sinon.stub(Number, 'isInteger').returns(currentAgeIsInteger);
+
+const randNum = Math.random();
+
+const spyOnMathRandom = sinon.stub(Math, 'random').returns(randNum);
+
+
+
+let newID = 1000000000 + oldID;
+
+const ageIsValid = Number.isInteger(currentAge);
+
+const randomNumber = Math.random() * 20;
+
+const randomInteger = Math.floor(randomNumber);
+
+const randomUserID = randomInteger + 1000000001;
