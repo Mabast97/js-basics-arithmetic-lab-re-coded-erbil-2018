337c893e0bc36f85a315b81dda1005404ab8404a
Mabast Hashm Jamel <mabasthashm1@gmail.com>
2018-07-28 18:50:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,15 @@ const spyOnMathRandom = sinon.stub(Math, 'random').returns(randNum);
 
 
 
-let newID = 1000000000 + oldID;
+/*
+Variables defined in the test suite:
+- oldID
+- currentAge
+You should NOT try to redefine any of the above variables.
+*/
+
+// Write your code below this line!
+const newID = 1000000000 + oldID;
 
 const ageIsValid = Number.isInteger(currentAge);
 
